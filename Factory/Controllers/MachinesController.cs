665bb06f61658c5553204875214118e71ff28207
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-19 14:27:35 -0700

    Add using directives and class for machines controller

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,21 @@
+using Factory.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+  }
+}
