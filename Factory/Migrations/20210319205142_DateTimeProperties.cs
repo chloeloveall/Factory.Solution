ccb4e171bffcdb8fec0b4ba23163d81009eec166
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-19 14:28:32 -0700

    Add initial migrations, hiredata migration, datetime migration

---
+++ Factory/Migrations/20210319205142_DateTimeProperties.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class DateTimeProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PurchaseDate",
+                table: "Machines",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RepairDate",
+                table: "Machines",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PurchaseDate",
+                table: "Machines");
+
+            migrationBuilder.DropColumn(
+                name: "RepairDate",
+                table: "Machines");
+        }
+    }
+}
