b4a3bfe0228a8743d0e19e1359ec0b247f8ea9ba
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-19 14:24:12 -0700

    Add using directive, add comments

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -20,10 +21,16 @@ namespace Factory.Models
     [Required]
     [Display(Name="Manufacturer")]
     public string MachineManufacturer { get; set; }
+    // this needs to only be connected to the create - it will essentially time stamp when the entry is created 
     [Display(Name="Purchase Date")]
     public DateTime PurchaseDate { get; set; }
+    // the repair sections can either be on a separate view or on the edit page - edit machine/add repair info
     [Display(Name="Last Repair Date")]
     public DateTime RepairDate { get; set; }
+    // this will be a dropdown with engineers - should be same engineers that have already been added to the machine - NOT the entire list of engineers  
+    [Display(Name="Last Repaired By")]
+    public string RepairedBy { get; set; }
+
     [Display(Name="Repair Log")]
     public string RepairLog { get; set; }
 
