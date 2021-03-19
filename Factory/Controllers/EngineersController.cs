475f7c440e976038305f2e7fa8b7b0d435634253
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-19 14:25:11 -0700

    Add using directives and class for engineers controller

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,20 @@
+using Factory.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+  }
+}
