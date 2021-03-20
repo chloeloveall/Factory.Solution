02d6dcc6b6859b90be4a4a2e5da2c4b722945a3f
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-20 04:09:45 -0700

    Add migration to add MachineStatus auto implemented property

---
+++ Factory/Migrations/20210320080412_MachineStatus.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class MachineStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MachineStatus",
+                table: "Machines",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MachineStatus",
+                table: "Machines");
+        }
+    }
+}
