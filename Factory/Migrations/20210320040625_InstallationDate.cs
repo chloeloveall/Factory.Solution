edb8e829cfb921ebd0109c37df294d67566beaee
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-19 21:38:21 -0700

    Add migration to update property name to InstallationDate

---
+++ Factory/Migrations/20210320040625_InstallationDate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class InstallationDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PurchaseDate",
+                table: "Machines",
+                newName: "InstallationDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "InstallationDate",
+                table: "Machines",
+                newName: "PurchaseDate");
+        }
+    }
+}
