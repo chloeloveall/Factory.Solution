3967be2d6222297ef0365c08c4f0fe5f49f2bbf9
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-03-21 19:17:52 -0700

    Add Dictionary to home controller to allow home view to access models

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,15 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    private readonly FactoryContext _db;
+    public HomeController(FactoryContext db)
     {
-
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
-
+      _db = db;
     }
+
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      var engineers = _db.Engineers.ToList();
+      var machines = _db.Machines.ToList();
+      Dictionary<string, object> model = new Dictionary<string, object> {};
+      model.Add("engineers", engineers);
+      model.Add("machines", machines);
+      return View(model);
+    }
+
+  }
 }
